e2d5e1ce8219e9f8da92daa9fbb1bc73eaa992f7
f1l1ph <filipmasarik04@gmail.com>
2024-07-17 16:08:51 +0200

    added navbar

---
+++ app/Components/NavBar.tsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <div className="bg-base-300 rounded-xl w-96 outline outline-primary">
+      <button className="btn btn-ghost text-xl">Home</button>
+      <button className="btn btn-ghost text-xl"> About me</button>
+    </div>
+  );
+};
+
+export default NavBar;
