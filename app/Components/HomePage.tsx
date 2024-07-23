8791a9381cd52e16fdf6f5a8423b520c7b356465
f1l1ph <50553234+f1l1ph@users.noreply.github.com>
2024-07-23 22:36:26 +0200

    changed first page

--- app/Components/HomePage.tsx
+++ app/Components/HomePage.tsx
@@ -3,25 +3,28 @@ import React from "react";
 const HomePage = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <div className="mockup-code mb-60">
-          <pre data-prefix="$" className="font-bold">
-            <code>Welcome</code>
-          </pre>
-          <pre data-prefix=">" className="text-warning">
-            <code>My name is Filip Masarik</code>
-          </pre>
-          <pre data-prefix=">" className="text-success">
-            <code>
-              and currently I am student at{" "}
-              <a
-                className="link link-primary"
-                href="https://www.fiit.stuba.sk/"
-              >
-                FIIT
-              </a>
-            </code>
-          </pre>
+      <div className="h-screen content-around">
+        <div className="text-center">
+          <h1 className="font-extrabold text-5xl">Welcome</h1>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <div className="mockup-code mb-60">
+            <pre data-prefix=">" className="text-warning">
+              <code>My name is Filip Masarik</code>
+            </pre>
+            <pre data-prefix=">" className="text-success">
+              <code>
+                and currently I am student at{" "}
+                <a
+                  className="link link-primary"
+                  href="https://www.fiit.stuba.sk/"
+                >
+                  FIIT
+                </a>
+              </code>
+            </pre>
+          </div>
         </div>
       </div>
     </>
