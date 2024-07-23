c825ee5756aef06d3a29d357764ac41604c1d801
f1l1ph <50553234+f1l1ph@users.noreply.github.com>
2024-07-23 20:17:19 +0200

    fixed the navbar and theme switch

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <>
       <div className="flex rounded items-cener justify-center pt-5">
-        <div className="bg-base-300 rounded-xl outline outline-primary hover:outline-secondary">
+        <div className="bg-base-300 rounded-xl outline outline-primary flex items-center justify-center hover:outline-secondary">
           <Link href="/">
             <button className="btn btn-ghost text-xl">Home</button>
           </Link>
@@ -17,10 +17,8 @@ const NavBar = () => {
           <Link href="/about">
             <button className="btn btn-ghost text-xl"> About me</button>
           </Link>
+          <ThemeToggle />
         </div>
-      </div>
-      <div className="flex justify-end items-center pt-5">
-        <ThemeToggle />
       </div>
     </>
   );
