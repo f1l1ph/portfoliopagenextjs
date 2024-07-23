7fd34c359288ea2111b5102c5ccb41da1c87eb52
f1l1ph <50553234+f1l1ph@users.noreply.github.com>
2024-07-23 19:44:35 +0200

    change the initial page

--- app/Components/HomePage.tsx
+++ app/Components/HomePage.tsx
@@ -3,17 +3,25 @@ import React from "react";
 const HomePage = () => {
   return (
     <>
-      <div className="hero min-h-screen">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="mockup-code mb-60">
+          <pre data-prefix="$" className="font-bold">
+            <code>Welcome</code>
+          </pre>
+          <pre data-prefix=">" className="text-warning">
+            <code>My name is Filip Masarik</code>
+          </pre>
+          <pre data-prefix=">" className="text-success">
+            <code>
+              and currently I am student at{" "}
+              <a
+                className="link link-primary"
+                href="https://www.fiit.stuba.sk/"
+              >
+                FIIT
+              </a>
+            </code>
+          </pre>
         </div>
       </div>
     </>
